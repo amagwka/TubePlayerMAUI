ebd6fc92cdb7aeecfaa252c13a194652252fb5e1
Mukhamejan <amagwka@gmail.com>
2023-06-06 07:50:47 +0600

    Add project files.

---
+++ Tube/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tube;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Tube/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Tube;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
